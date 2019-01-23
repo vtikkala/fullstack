18b4651c7be125e891a461347a8170b722404dee
root <root@1BQQ6XW.keskusta.dhcp.hupnet.helsinki.fi>
2019-01-23 19:04:45 +0200

    Tehtävä 1.1 valmis

--- Tehtavat/Osa 1/osa1/src/index.js
+++ Tehtavat/Osa 1/osa1/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <>
+            <p>{props.part1} {props.exercises1}</p>
+            <p>{props.part2} {props.exercises2}</p>
+            <p>{props.part3} {props.exercises3}</p>
+        </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <>
+            <p>yhteensä {props.total} tehtävää</p>
+        </>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
     const part1 = 'Reactin perusteet'
@@ -12,17 +38,16 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+            <Header course={course} />
+            <Content 
+                part1={part1}
+                part2={part2}
+                part3={part3}
+                exercises1={exercises1}
+                exercises2={exercises2} 
+                exercises3={exercises3} 
+            />   
+            <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     )
 }
