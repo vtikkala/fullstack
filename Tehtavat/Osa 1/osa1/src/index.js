90c2eff9a39ae6cb4cda6907703423ec6f28fa34
root <root@localhost.localdomain>
2019-01-27 14:27:30 +0200

    Tehtävä 1.3 valmis

--- Tehtavat/Osa 1/osa1/src/index.js
+++ Tehtavat/Osa 1/osa1/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => {
-    return (
-        <>
-            <h1>{props.course}</h1>
-        </>
-    )
+    console.log(props)
+    return <h1>{props.course}</h1>
 }
 
 const Content = (props) => {
@@ -14,64 +11,57 @@ const Content = (props) => {
         <>
             <Part1 part1={props.part1} exercises1={props.exercises1} />
             <Part2 part2={props.part2} exercises2={props.exercises2} />
-            <Part3 part3={props.part3} exercises1={props.exercises3} />
-        </>
-    )
-}
-
-const Total = (props) => {
-    return (
-        <>
-            <p>yhteensä {props.total} tehtävää</p>
+            <Part3 part3={props.part3} exercises3={props.exercises3} />
         </>
     )
 }
 
 const Part1 = (props) => {
-    return (
-        <>
-            <p>{props.part1} {props.exercises1}</p>
-        </>
-    )
+    return <p>{props.part1} {props.exercises1}</p>
 }
 
 const Part2 = (props) => {
-    return (
-        <>
-            <p>{props.part2} {props.exercises2}</p>
-        </>
-    )
+    return <p>{props.part2} {props.exercises2}</p>
 }
 
 const Part3 = (props) => {
-    return (
-        <>
-            <p>{props.part3} {props.exercises3}</p>
-        </>
-    )
+    return <p>{props.part3} {props.exercises3}</p>
+}
+
+const Total = (props) => {
+    return <p>yhteensä {props.total} tehtävää</p>
 }
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+    
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10
+    }
+
+    const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 10
+    }
+    
+    const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
             <Content 
-                part1={part1}
-                part2={part2}
-                part3={part3}
-                exercises1={exercises1}
-                exercises2={exercises2} 
-                exercises3={exercises3} 
+                part1={part1.name}
+                part2={part2.name}
+                part3={part3.name}
+                exercises1={part1.exercises}
+                exercises2={part2.exercises} 
+                exercises3={part3.exercises} 
             />   
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Total total={part1.exercises + part2.exercises + part3.exercises} />
         </div>
     )
 }
