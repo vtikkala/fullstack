d41049743e95f80046cbc7030269a138c500829d
root <root@localhost.localdomain>
2019-01-28 18:39:13 +0200

    Tehtävä 1.7 valmis

--- Tehtavat/Osa 1/osa1/src/index.js
+++ Tehtavat/Osa 1/osa1/src/index.js
@@ -7,9 +7,17 @@ const Header = (props) => {
 
 const Result = (props) => {
     return (
-        <p>
-            {props.result} {props.clicks}
-        </p>
+        <>
+            {props.result} {props.clicks} <br />
+        </>
+    )
+}
+
+const Positive = (props) => {
+    return (
+        <>
+            {props.name} {(props.value * 100).toFixed(0) + ' %'} 
+        </>
     )
 }
 
@@ -42,9 +50,13 @@ const App = () => {
             <button onClick={() => setToBad(bad + 1)}>huono</button>
 
             <Header header={statistics} />
-            <Result result='Hyvä' clicks={good} />
-            <Result result='Neutraali' clicks={neutral} />
-            <Result result='Huono' clicks={bad} />
+            
+            <Result result='hyvä' clicks={good} />
+            <Result result='neutraali' clicks={neutral} />
+            <Result result='huono' clicks={bad} />
+            <Result result='yhteensä' clicks={good + neutral + bad} />
+            <Result result='keskiarvo' clicks={(good * 1 + neutral *0 + bad * (-1)) / (good + neutral + bad)} />
+            <Positive name='positiivisia' value={good / (good + neutral + bad)} />
         </div>
     )
 }
