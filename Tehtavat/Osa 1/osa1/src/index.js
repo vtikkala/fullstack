52e99ff04600d3892f399641ab6bb135d24d893c
root <root@localhost.localdomain>
2019-01-28 19:27:35 +0200

    Tehtävä 1.10 valmis

--- Tehtavat/Osa 1/osa1/src/index.js
+++ Tehtavat/Osa 1/osa1/src/index.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return <h1>{props.header}</h1> 
 }
 
-const Result = (props) => {
+const Statistic = (props) => {
     return (
         <>
             {props.result} {props.clicks} <br />
@@ -24,6 +24,10 @@ const Positive = (props) => {
 const NoResult = () => {
     return <p>Ei yhtään palautetta annettu</p>
 }
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text} </button>
+)
 
 const App = () => {
     const header = 'anna palautetta'
@@ -45,17 +49,27 @@ const App = () => {
         setBad(newValue)
     }
 
+    const handleGoodClick = () => {
+        setToGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setToNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setToBad(bad + 1)
+    }
+
     if ((good + neutral + bad) == 0) {
         return (
             <div>
                 <Header header={header} />
-                
-                <button onClick={() => setToGood(good + 1)}>hyvä</button>
-                <button onClick={() => setToNeutral(neutral + 1)}>neutraali</button>
-                <button onClick={() => setToBad(bad + 1)}>huono</button>
-    
+                <Button handleClick={handleGoodClick} text='hyvä'/>
+                <Button handleClick={handleNeutralClick} text='neutraali' />
+                <Button handleClick={handleBadClick} text='huono' />    
                 <Header header={statistics} />
-                <p>Ei yhtään palautetta annettu</p>
+                <NoResult />
             </div>
         )
     } else {
@@ -69,11 +83,11 @@ const App = () => {
                 <button onClick={() => setToBad(bad + 1)}>huono</button>
 
                 <Header header={statistics} />
-                <Result result='hyvä' clicks={good} />
-                <Result result='neutraali' clicks={neutral} />
-                <Result result='huono' clicks={bad} />
-                <Result result='yhteensä' clicks={good + neutral + bad} />
-                <Result result='keskiarvo' clicks={(good * 1 + neutral *0 + bad * (-1)) / (good + neutral + bad)} />
+                <Statistic result='hyvä' clicks={good} />
+                <Statistic result='neutraali' clicks={neutral} />
+                <Statistic result='huono' clicks={bad} />
+                <Statistic result='yhteensä' clicks={good + neutral + bad} />
+                <Statistic result='keskiarvo' clicks={(good * 1 + neutral *0 + bad * (-1)) / (good + neutral + bad)} />
                 <Positive name='positiivisia' value={good / (good + neutral + bad)} />
             </div>
         )
