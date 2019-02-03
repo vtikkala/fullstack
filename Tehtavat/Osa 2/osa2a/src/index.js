9775edc9194eb322b63783fbdceed72a0c2f5545
root <root@localhost.localdomain>
2019-02-03 16:30:36 +0200

    Tehtävä 2.2. valmis

--- Tehtavat/Osa 2/osa2a/src/index.js
+++ Tehtavat/Osa 2/osa2a/src/index.js
@@ -1,74 +1,48 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-    const { header } = props
-    return (
-        <>
-            <h1>{header}</h1>
-        </>
-    )
-}
 
-const Content = (props) => {
-    const { content } = props
-    return (
-        <>
-            <Part1 part1={content.part1} exercises1={content.exercises1} />
-            <Part2 part2={content.part2} exercises2={content.exercises2} />
-            <Part3 part3={content.part3} exercises1={content.exercises3} />
-        </>
-    )
-}
-
-const Total = (props) => {
-    return (
-        <>
-            <p>yhteensä {props.total} tehtävää</p>
-        </>
-    )
-}
-
-const Part1 = (props) => {
-    return (
-        <>
-            <p>{props.part1} {props.exercises1}</p>
-        </>
-    )
-}
-
-const Part2 = (props) => {
-    return (
-        <>
-            <p>{props.part2} {props.exercises2}</p>
-        </>
-    )
-}
-
-const Part3 = (props) => {
-    return (
-        <>
-            <p>{props.part3} {props.exercises3}</p>
-        </>
-    )
-}
-
-const Course = (props) => {
-    console.log(props)
-    
+const Course = (props) => {    
     return (
         <>
             <Header header = {props.course.name} />
-            <Content content = {props.course.parts} />
+            <Content contents = {props.course.parts} />
+            <Total totals = {props.course.parts} />            
+        </>
+    )
+}
 
-            <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-            <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
-            <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
+const Header = (props) => {
+    const { header } = props
+    return <h1>{header}</h1>
+}
+
+const Content = (props) => {
+    const { contents } = props
+    
+    const rows = () => contents.map(content => 
+        <p key={content.id}>
+            {content.name} {content.exercises}
+        </p>)
+    
+    return (
+        <>
+            {rows()}
         </>
     )
 
 }
 
+const Total = (props) => {
+    const { totals } = props
+    let sum = 0
+
+    totals.forEach(total => {
+        sum = sum + total.exercises
+    });
+
+    return <p>yhteensä {sum} tehtävää</p>
+}
 
 const App = () => {
     const course = {
@@ -88,6 +62,11 @@ const App = () => {
                 name: 'Komponenttien tila',
                 exercises: 14,
                 id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 7,
+                id: 4
             }
         ]
     }
